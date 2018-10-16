f630436f36132df12adfb0bd96bc57bcb2c5840e
ANS <36783010+answebdev@users.noreply.github.com>
2018-10-15 18:26:15 -0700

    installed and required packages

--- keys.js
+++ keys.js
@@ -4,3 +4,13 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsintown = {
+    api: process.env.API,
+    id: process.env.APP_ID
+};
+
+exports.ombd = {
+    api: process.env.OMBD_API,
+    key: process.env.KEY
+};
--- liri.js
+++ liri.js
@@ -1,1 +1,18 @@
-require("dotenv").config();
+// Use .env to hide keys
+var dotenv = require('dotenv').config();
+
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var moment = require('moment');
+var fs = require('fs'); 
+var command = process.argv[2];
+var searchTerm = process.argv[3];
+
+
+// Access Spotify Keys
+var spotify = new Spotify(keys.spotify);
+
+// Grab command from the terminal
+var command = process.argv[2];
+var searchValue = "";
