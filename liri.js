c4d1ced495d65ff7064f598418c2a650dcdec782
ANS <36783010+answebdev@users.noreply.github.com>
2018-10-17 20:29:02 -0700

    added logic to output data for Mr. Nobody if no movie is typed in

--- liri.js
+++ liri.js
@@ -68,6 +68,11 @@ function concertThis() {
 function spotify() {
   var command = process.argv[2];
   var songTitle = process.argv[3];
+
+  // if (!songTitle) {
+  //   songTitle = "The Sign";
+  // }
+
   // Access Spotify Keys
   var spotify = new Spotify(keys.spotify);
 
@@ -89,49 +94,53 @@ function spotify() {
   })
 }
 
-  // ========================================================
-  // OMDB CODE
-  function movieThis() {
-    var command = process.argv[2];
-    var movieName = process.argv[3];
+// ========================================================
+// OMDB CODE
+function movieThis() {
+  var command = process.argv[2];
+  var movieName = process.argv[3];
 
-    // Make a request to the OMDB API with the movie specified
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    // var defaultUrl = "http://www.omdbapi.com/?t=mr+nobody+&y=&plot=short&apikey=trilogy";
-
-    request(queryUrl, function (error, response, body) {
-
-      // If the request is successful
-      if (!error && response.statusCode === 200) {
-
-        // Parse the body of the site and recover the following results
-        // console.log("COMMAND: " + command);
-
-        console.log("\n*************** OMDB SEARCH RESULT ****************\n");
-        console.log("Title of Movie: " + JSON.parse(body).Title);
-        console.log("Release Year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-        console.log("Country where the movie was produced: " + JSON.parse(body).Country);
-        console.log("Language: " + JSON.parse(body).Language);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Actors: " + JSON.parse(body).Actors);
-        console.log("\n***************************************************\n");
-      }
-    });
+  if (!movieName) {
+    movieName = "Mr. Nobody";
   }
 
-  // ========================================================
-  // DO WHAT IT SAYS CODE
-  function doWhatItSays() {
-    fs.writeFile("random.txt", 'spotify-this-song,"I Want it That Way"', function (err) {
-      var song = "spotify-this-song 'I Want it That Way'"
-      // If the code experiences any errors it will log the error to the console.
-      if (err) {
-        return console.log(err);
-      };
-      // Otherwise, it will print: "random.txt was updated!"
-      console.log("random.txt was updated!");
-    });
-  };
+  // Make a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  // var defaultUrl = "http://www.omdbapi.com/?t=mr+nobody+&y=&plot=short&apikey=trilogy";
+
+  request(queryUrl, function (error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover the following results
+      // console.log("COMMAND: " + command);
+
+      console.log("\n*************** OMDB SEARCH RESULT ****************\n");
+      console.log("Title of Movie: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("\n***************************************************\n");
+    }
+  });
+}
+
+// ========================================================
+// DO WHAT IT SAYS CODE
+function doWhatItSays() {
+  fs.writeFile("random.txt", 'spotify-this-song,"I Want it That Way"', function (err) {
+    var song = "spotify-this-song 'I Want it That Way'"
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+      return console.log(err);
+    };
+    // Otherwise, it will print: "random.txt was updated!"
+    console.log("random.txt was updated!");
+  });
+};
 
